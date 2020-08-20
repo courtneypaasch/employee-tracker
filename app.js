78b2f259b98c3b2868370bf7f11b79fe5251ec04
courtneypaasch <paasch.courtney@gmail.com>
2020-08-19 21:58:17 -0700

    add init function, rename prompts to UserPrompts for clarity

--- app.js
+++ app.js
@@ -4,12 +4,64 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const UserPrompts = require("./lib/UserPrompts");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let team = [];
+let employee;
+
+function employeeType(addMore){
+    if(addMore.addEmployee === "Manager"){
+        return UserPrompts.managerPrompt;
+    } else if(addMore.addEmployee === "Engineer") {
+        return UserPrompts.engineerPrompt;
+    } else if(addMore.addEmployee === "Intern"){
+        return UserPrompts.internPrompt;
+    }
+}
+
+
+async function init() {
+    try{
+        const addMore = await inquirer.prompt(UserPrompts.addMore);
+
+        if(addMore.addEmployee === "No more team members"){
+           fs.writeFile(outputPath, render(team), function(error){
+               if(error){
+                   return console.log(error);
+               }else {
+                   console.log("Team saved in new file!");
+               }
+           });
+           return;
+            
+        }else{
+            const choice = await employeeType(addMore);
+            const employeePrompts = await inquirer.prompt(UserPrompts.employeePrompts);
+            const rolePrompt = await inquirer.prompt(choice);
+            
+            if(addMore.addEmployee === "Manager"){
+                employee = new Manager(employeePrompts.name, employeePrompts.id, employeePrompts.email, rolePrompt.officeNumber)
+            }  else if(addMore.addEmployee === "Engineer"){
+                employee = new Engineer(employeePrompts.name, employeePrompts.id, employeePrompts.email, rolePrompt.github)
+            } else if(addMore.addEmployee === "Intern"){
+                employee = new Manager(employeePrompts.name, employeePrompts.id, employeePrompts.email, rolePrompt.school)
+            }
+            
+            team.push(employee);
+            init()
+        
+        }
+    }catch (error) {
+        console.log(error);
+    }
+}
+
+init()
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
