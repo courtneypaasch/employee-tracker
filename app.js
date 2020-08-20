578f31c90d3af4baeb2237f345c9106a4eda2611
courtneypaasch <paasch.courtney@gmail.com>
2020-08-19 22:58:23 -0700

    ensure first prompt is always manager

--- app.js
+++ app.js
@@ -14,50 +14,95 @@ const render = require("./lib/htmlRenderer");
 let team = [];
 let employee;
 
-function employeeType(addMore){
-    if(addMore.addEmployee === "Manager"){
+
+function employeeType(addMore) {
+    if (addMore.addEmployee === "Manager") {
         return UserPrompts.managerPrompt;
-    } else if(addMore.addEmployee === "Engineer") {
+    } else if (addMore.addEmployee === "Engineer") {
         return UserPrompts.engineerPrompt;
-    } else if(addMore.addEmployee === "Intern"){
+    } else if (addMore.addEmployee === "Intern") {
         return UserPrompts.internPrompt;
     }
 }
 
-
+let firstloop = 0;
 async function init() {
-    try{
-        const addMore = await inquirer.prompt(UserPrompts.addMore);
 
-        if(addMore.addEmployee === "No more team members"){
-           fs.writeFile(outputPath, render(team), function(error){
-               if(error){
-                   return console.log(error);
-               }else {
-                   console.log("Team saved in new file!");
-               }
-           });
-           return;
-            
-        }else{
-            const choice = await employeeType(addMore);
-            const employeePrompts = await inquirer.prompt(UserPrompts.employeePrompts);
-            const rolePrompt = await inquirer.prompt(choice);
-            
-            if(addMore.addEmployee === "Manager"){
-                employee = new Manager(employeePrompts.name, employeePrompts.id, employeePrompts.email, rolePrompt.officeNumber)
-            }  else if(addMore.addEmployee === "Engineer"){
-                employee = new Engineer(employeePrompts.name, employeePrompts.id, employeePrompts.email, rolePrompt.github)
-            } else if(addMore.addEmployee === "Intern"){
-                employee = new Manager(employeePrompts.name, employeePrompts.id, employeePrompts.email, rolePrompt.school)
-            }
-            
-            team.push(employee);
-            init()
-        
+    if (firstloop === 0) {
+        const newTeam = await inquirer.prompt(UserPrompts.newTeam);
+
+        if (newTeam.team === "No") {
+            return console.log("No new team created.")
         }
-    }catch (error) {
-        console.log(error);
+        else {
+
+            try {
+
+                const managerPrompt = await inquirer.prompt(UserPrompts.managerPrompt);
+                const addMore = await inquirer.prompt(UserPrompts.addMore);
+
+                employee = new Manager(managerPrompt.name, managerPrompt.id, managerPrompt.email, managerPrompt.officeNumber);
+
+                if (addMore.addEmployee === "No more team members") {
+                    fs.writeFile(pathtofile, render(team), function (error) {
+                        if (error) {
+                            return console.log(error);
+                        } else {
+                            console.log("Team saved in new file!");
+                        }
+                    });
+                    return;
+
+                } else {
+                    const choice = await employeeType(addMore);
+                    const employeePrompts = await inquirer.prompt(UserPrompts.employeePrompts);
+                    const rolePrompt = await inquirer.prompt(choice);
+
+                    if (addMore.addEmployee === "Engineer") {
+                        employee = new Engineer(employeePrompts.name, employeePrompts.id, employeePrompts.email, rolePrompt.github);
+                    } else if (addMore.addEmployee === "Intern") {
+                        employee = new Manager(employeePrompts.name, employeePrompts.id, employeePrompts.email, rolePrompt.school);
+                    }
+                    firstloop = 1;
+                    team.push(employee);
+                    init()
+                }
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    } else {
+        try {
+            const addMore = await inquirer.prompt(UserPrompts.addMore);
+
+            if (addMore.addEmployee === "No more team members") {
+                fs.writeFile(pathtofile, render(team), function (error) {
+                    if (error) {
+                        return console.log(error);
+                    } else {
+                        console.log("Team saved in new file!");
+                    }
+                });
+                return;
+
+            } else {
+                const choice = await employeeType(addMore);
+                const employeePrompts = await inquirer.prompt(UserPrompts.employeePrompts);
+                const rolePrompt = await inquirer.prompt(choice);
+
+                if (addMore.addEmployee === "Engineer") {
+                    employee = new Engineer(employeePrompts.name, employeePrompts.id, employeePrompts.email, rolePrompt.github);
+                } else if (addMore.addEmployee === "Intern") {
+                    employee = new Manager(employeePrompts.name, employeePrompts.id, employeePrompts.email, rolePrompt.school);
+                }
+                team.push(employee);
+                init()
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
